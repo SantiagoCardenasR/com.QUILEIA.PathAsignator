98fd63f68db8799ccd3217e36b155f584ee76b6f
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2020-09-14 11:25:34 -0500

    Update ServiceOfficerControllerTest.java

--- src/test/java/com/QUILEIA/app/ServiceOfficerControllerTest.java
+++ src/test/java/com/QUILEIA/app/ServiceOfficerControllerTest.java
@@ -1,6 +1,5 @@
 package com.QUILEIA.app;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
@@ -9,19 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RequestCallback;
-import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
 import com.QUILEIA.app.model.Officer;
@@ -35,8 +27,6 @@ class ServiceOfficerControllerTest {
 	private RestTemplate restTemplate;
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	@Autowired
-	private OfficerService officersService;
 	
 	@Test
 	@Sql("./officers_insert.sql")
